578606596ec19b5363a505e8690e31ec3da28f4b
Ravi Parmar <ravi.parmar@simulas.in>
2024-10-22 07:01:37 +0000

    jsp logic added

--- hotelpro-backend/index.js
+++ hotelpro-backend/index.js
@@ -1,66 +1,56 @@
+import "./config.js";
 import express from "express";
 import cors from "cors";
-import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
 import morganMiddleware from "./logger/morgan.logger.js";
-import mongo from "./database/database.service.js";
 import { errorHandler } from "./middleware/error.middlewares.js";
-import indexRouter from "./routes/index.routes.js";
-import http from "http";
 import path from "path";
+const app = express();
+
+const corsOptions = {
+  origin:
+    process.env.CORS_ORIGIN === "*"
+      ? "*" // This might give CORS error for some origins due to credentials set to true
+      : process.env.CORS_ORIGIN?.split(","), // For multiple cors origin for production.
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: false, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(morganMiddleware);
 import { fileURLToPath } from "url"; // Import fileURLToPath for ES modules
 
-configDotenv();
-const app = express();
-console.log(process.env.APP_PORT);
-const port = process.env.APP_PORT || 8080; // Default to 8080 if not set
-
-// Create HTTP server with Express app
-const server = http.createServer(app);
-
-// Create __dirname equivalent in ES module
 const __filename = fileURLToPath(import.meta.url); // Get the current module's URL
-const __dirname = path.dirname(__filename); // Get the directory name
+const __dirname = path.dirname(__filename);
+import indexRouter from "./routes/index.routes.js";
 
-// Global middlewares
-app.use(
-  cors({
-    origin:
-      process.env.CORS_ORIGIN === "*"
-        ? "*"
-        : process.env.CORS_ORIGIN?.split(","),
-    credentials: true,
-  })
-);
-
-app.use(
-  express.json({
-    limit: "16kb",
-  })
-);
-app.use(cookieParser());
-
-app.use(morganMiddleware);
-
+//routes declaration
 app.use("/hotelpro", indexRouter);
-
-// Serve static files from dist directory
 const distDir = path.join(__dirname, "dist", "browser"); // Use the new __dirname
 app.use(express.static(distDir));
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(distDir, "index.html"));
 });
-
 // Handling preflight requests
-app.options("*", cors());
+// preflight requests sent by the browser to determine whether the actual request (e.g., a GET or POST request) is safe to send.
+app.options(
+  "*",
+  cors({
+    origin: true,
 
-// Set the view engine to ejs
+    credentials: true,
+  })
+);
 app.set("view engine", "ejs");
-
-// Error handler middleware
 app.use(errorHandler);
+const port = process.env.PORT || 8000;
 
-// Start the server and listen on the specified port
-server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
+app.get("/zz", (req, res) => {
+  res.send("Hello World!");
 });
